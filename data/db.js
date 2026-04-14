2e07a23e0c204c1398ed5e517a18e0d039e4f797
Mohamad Bader almsaddi alzin <baderalmsaddy@gmail.com>
2026-04-14 11:56:30 +0200

    added db.js path and grouped table creation in the same file, added router express to inventory and members

--- data/db.js
+++ data/db.js
@@ -1,7 +1,33 @@
 import Database from 'better-sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const db = new Database('./book_library.db');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dbPath = path.join(__dirname, 'books.db');
+const db = new Database(dbPath);
 
-console.log('Connected to SQLite database');
+db.exec(`
+  CREATE TABLE IF NOT EXISTS authors (
+    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT
+  );
+
+  CREATE TABLE IF NOT EXISTS books (
+    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    isbn TEXT,
+    publication_year INTEGER,
+    genre TEXT,
+    author_id INTEGER,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  );
+
+  CREATE TABLE IF NOT EXISTS members (
+    member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
+  );
+`);
 
 export default db;
