8eceba23ac7e79164568a5d8ab4626206e3e3128
baraah <baraah.alshiaani@gmail.com>
2026-04-17 01:26:13 +0200

    added featues for loans and members

--- src/routes/loans.js
+++ src/routes/loans.js
@@ -32,6 +32,7 @@ INSERT OR IGNORE INTO members (member_id, name, email)
 VALUES (1, 'Test User', 'test@test.com');
 `).run();
 
+//loan a book
 router.post('/', (req, res) => {
   const { book_id, member_id } = req.body;
 
@@ -39,19 +40,48 @@ router.post('/', (req, res) => {
     return res.status(400).json({ error: 'Missing data' });
   }
 
-  const stmt = db.prepare(`
-    INSERT INTO loans (book_id, member_id, loan_date)
-    VALUES (?, ?, datetime('now'))
-  `);
+  try {
+    //check if book is already loaned
+    const existingLoan = db.prepare(`
+      SELECT loan_id FROM loans
+      WHERE book_id = ? AND returned = 0
+    `).get(book_id);
 
-  const result = stmt.run(book_id, member_id);
+    if (existingLoan) {
+      return res.status(409).json({
+        error: 'This book is already loaned'
+      });
+    }
 
-  res.json({
-    message: 'Book loaned successfully',
-    loanId: result.lastInsertRowid
-  });
+    const result = db.prepare(`
+      INSERT INTO loans (book_id, member_id, loan_date)
+      VALUES (?, ?, datetime('now'))
+    `).run(book_id, member_id);
+
+    res.json({
+      message: 'Book loaned successfully',
+      loanId: result.lastInsertRowid
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
+  //const stmt = db.prepare(`
+//INSERT INTO loans (book_id, member_id, loan_date)
+   // VALUES (?, ?, datetime('now'))
+  //`);
+
+  //const result = stmt.run(book_id, member_id);
+
+  //res.json({
+   // message: 'Book loaned successfully',
+    //loanId: result.lastInsertRowid
+  //});
+//});
+
+//return a book
 router.patch('/return/:id', (req, res) => {
   const { id } = req.params;
 
@@ -73,6 +103,7 @@ router.patch('/return/:id', (req, res) => {
   });
 });
 
+//get all active loan
 router.get('/', (req, res) => {
   const stmt = db.prepare(`
     SELECT 
@@ -91,23 +122,7 @@ router.get('/', (req, res) => {
   res.json(loans);
 });
 
-export default router;
-router.get('/', (req, res) => {
-  const stmt = db.prepare(`
-    SELECT 
-      l.loan_id,
-      b.title,
-      m.name,
-      l.loan_date
-    FROM loans l
-    JOIN books b ON l.book_id = b.book_id
-    JOIN members m ON l.member_id = m.member_id
-    WHERE l.returned = 0
-  `);
-
-  const loans = stmt.all();
-  res.json(loans);
-});
+//get loan history for a specific book 
 router.get('/book/:id', (req, res) => {
   const { id } = req.params;
 
@@ -126,3 +141,7 @@ router.get('/book/:id', (req, res) => {
   const history = stmt.all(id);
   res.json(history);
 });
+
+export default router;
+
+
