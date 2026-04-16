5cba8ecf5d3a9ef970dd95e5a865a0fce1a405ef
Mustafa Homsi <homsi6313@gmail.com>
2026-04-17 01:18:32 +0200

    clean loans tests (no data duplication)

---
+++ tests/loan.test.js
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import app from '../src/index.js';
+
+describe('Loans API', () => {
+
+  it('should get loans list', async () => {
+    const res = await request(app).get('/loans');
+
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
+  it('should get loan history for a book', async () => {
+    const res = await request(app).get('/loans/book/9');
+
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
+});
