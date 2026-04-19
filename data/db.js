74987eac02c3034d5de0f3b5d56a3f085ed5e5e0
baraah <baraah.alshiaani@gmail.com>
2026-04-19 23:02:31 +0200

    updated db.js auto create the DB if it doesnt exist

--- data/db.js
+++ data/db.js
@@ -5,12 +5,11 @@ const db = new Database('./data/book_library.db');
 
 console.log('Connected to database');
 
-const schemaPath = './book_library.sql';
+const schemaPath = './data/books_library.sql';
 
 if (fs.existsSync(schemaPath)) {
   const schema = fs.readFileSync(schemaPath, 'utf8');
   db.exec(schema);
-  
 }
 
 export default db;
